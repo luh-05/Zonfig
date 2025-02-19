77c65cdc162b5f3b43a90cef4a2697c1432fda47
Luh-code <Lasse@hueffler.de>
2025-02-19 13:26:30 +0100

    pointer fix

--- src/zonfig.zig
+++ src/zonfig.zig
@@ -2,26 +2,20 @@ const std = @import("std");
 
 const convert = @import("./convert.zig");
 
-fn createConfigModule(
-    b: *std.Build,
-    target: *std.Build.ResolvedTarget,
-    optimize: *std.builtin.OptimizeMode,
-    path: []const u8,
-    name: []const u8
-) !*std.Build.Module {
+fn createConfigModule(b: *std.Build, target: *std.Build.ResolvedTarget, optimize: *std.builtin.OptimizeMode, path: []const u8, name: []const u8) !*std.Build.Module {
     const zig_path = try convert.convertFile(b.allocator, path, name);
     defer b.allocator.free(zig_path);
     const module = b.addModule(name, .{
         .root_source_file = b.path(zig_path),
         .target = target,
         .optimize = optimize,
-    }); 
+    });
 
     return module;
 }
 
 pub fn addConfig(b: *std.Build, module: *std.Build.Module, path: []const u8, name: []const u8) !void {
-    const config_module = try createConfigModule(b, module.resolved_target.?, module.optimize.?, path, name);
+    const config_module = try createConfigModule(b, &module.resolved_target.?, &module.optimize.?, path, name);
 
     module.addImport(name, config_module);
 }
