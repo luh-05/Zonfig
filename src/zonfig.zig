7b70c6a64a40794eafec0f9b76e39ac49d0e04bc
Luh-code <Lasse@hueffler.de>
2025-02-19 13:33:51 +0100

    im stupid

--- src/zonfig.zig
+++ src/zonfig.zig
@@ -7,15 +7,15 @@ fn createConfigModule(b: *std.Build, target: std.Build.ResolvedTarget, optimize:
     defer b.allocator.free(zig_path);
     const module = b.addModule(name, .{
         .root_source_file = b.path(zig_path),
-        .target = target.?,
-        .optimize = optimize.?,
+        .target = target,
+        .optimize = optimize,
     });
 
     return module;
 }
 
 pub fn addConfig(b: *std.Build, module: *std.Build.Module, path: []const u8, name: []const u8) !void {
-    const config_module = try createConfigModule(b, module.resolved_target, module.optimize, path, name);
+    const config_module = try createConfigModule(b, module.resolved_target.?, module.optimize.?, path, name);
 
     module.addImport(name, config_module);
 }
